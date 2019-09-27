721603594adae1ba7b64933fdda20e788aab48be
kmietotwy <kmietotwy@gmail.com>
2019-09-27 12:28:14 -0700

    fetching data for pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e17b1a5fea5486475d50";
+/******/ 	var hotCurrentHash = "79eb47778eddb1127aa7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
